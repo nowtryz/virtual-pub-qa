1b04c150fbb34802b753174a6e45bff564417216
Nowtryz <nowtryz.dev@gmail.com>
2021-03-05 11:06:57 +0100

    :tada: Initial Commit

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pub","l":"Bar"},{"p":"pub","l":"Boisson"},{"p":"pub","l":"BoissonTest"},{"p":"pub","l":"Cave"},{"p":"pub","l":"Cocktail"},{"p":"pub","l":"Pub"}]
